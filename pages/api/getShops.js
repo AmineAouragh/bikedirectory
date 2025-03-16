65f0e535c2335dc3bd6c6ea0ad37a79776293869
AMINEAOURAGH <amine.aouragh.20@gmail.com>
2025-03-16 18:22:10 +0000

    fix

--- pages/api/getShops.js
+++ pages/api/getShops.js
@@ -1,24 +1,26 @@
 import { supabase } from "./supabase"
 
 export default async function handler(req, res){
+  if (req.method == "GET"){
     try {
-        const { data: bike_shops, error } = await supabase
-        .from('bike_shops')
-        .select(`
-            *, 
-            bike_types (
-              bike_type
-            ),
-            bike_rental_options (
-              rental_duration
-            )
-        `)
+      const { data: bike_shops, error } = await supabase
+      .from('bike_shops')
+      .select(`
+          *, 
+          bike_types (
+            bike_type
+          ),
+          bike_rental_options (
+            rental_duration
+          )
+      `)
 
-        if (error) throw error 
+      if (error) throw error 
 
-        res.status(200).json({ message: "Bike shops fetched successfully: ", bike_shops })
+      res.status(200).json({ message: "Bike shops fetched successfully: ", bike_shops })
     } catch (error) {
-        console.error("Error while fetching bike shops: ", error.message)
-        res.status(500).json({ message: "Server error", error: error.message })
+      console.error("Error while fetching bike shops: ", error.message)
+      res.status(500).json({ message: "Server error", error: error.message })
     }
+  }
 }

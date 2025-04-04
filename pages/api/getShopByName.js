00b70c7353585997db49dc99f274c973af5b3243
AMINEAOURAGH <amine.aouragh.20@gmail.com>
2025-04-04 20:08:51 +0000

    add serverless function to fetch bike shop by name

---
+++ pages/api/getShopByName.js
@@ -0,0 +1,38 @@
+import { supabase } from "./supabase"
+
+export default async function handler(req, res){
+
+  if (req.method === "GET"){
+    try {
+
+      const shop_name = req.query.shop_name
+      console.log("Shop name value used in supabase: ", shop_name)
+
+      const { data: bike_shops, error } = await supabase
+      .from('bike_shops')
+      .select(`
+          *, 
+          bike_types (
+            bike_type
+          ),
+          bike_rental_options (
+            rental_duration
+          ),
+          bike_shop_hours (
+            day,
+            opening_hour,
+            closing_hour, 
+            closed
+          )
+      `)
+      .eq('shop_name', shop_name)
+
+      if (error) throw error 
+
+      return res.status(200).json({ bike_shops })
+    } catch (error) {
+      console.error("Error while fetching bike shop: ", error.message)
+      res.status(500).json({ message: "Server error", error: error.message })
+    }
+  }
+}
